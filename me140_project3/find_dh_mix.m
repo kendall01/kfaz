0f3c5986644f10cc6306f29f38bd8a6c1d0a85d8
Kendall <kendallf01@gmail.com>
2016-04-19 14:50:09 -0700

    walking through adiabatic flame

--- me140_project3/find_dh_mix.m
+++ me140_project3/find_dh_mix.m
@@ -1,10 +1,11 @@
 function [ dh ] = find_dh_mix( T1,T2,AF )
 %Returns kJ
+AFs = 14.43;                    % stoichiometric Air-Fuel-Ratio, found from balancing equation (LEC 5, slides 3-4)
 
 f_temp = @(x,y) sp_heats_mix(x,y); %Note: Cp is the first element in the return of sp_heats and this returns just cp as a 1x1 double
 dh = zeros(size(T1,1),1);
 for i = 1:size(T1,1)
-    dh(i) = integral( (@(x)f_temp(x,AF(i))) ,T1(i),T2(i));
+    dh(i) = integral( (@(x)f_temp(x,AFs./AF(i))) ,T1(i),T2(i));
 end           
 end
 
